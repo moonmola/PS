abd6aa6a3df3f224c1230f590ab586fee0a3b40f
moon722 <miz722@khu.ac.kr>
2020-07-24 23:39:40 +0900

    7/24 9012

---
+++ baekjoon/else/9012.cpp
@@ -0,0 +1,53 @@
+//
+// 9012.cpp
+// coding
+//
+// Created by mooninzoo on 2020-07-23.
+// Copyright (c) 2020 mooninzoo. All rights reserved
+//
+
+/*
+ * 괄호
+ * 맞았다!
+ *
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int left =0, right =0,t;
+    string brac;
+    cin >> t;
+    for(int i = 0; i<t; i++){
+        cin >> brac;
+//        cout << brac.size() << endl;
+        for(int j = 0; j <brac.size();j++){
+            if(brac[j] == '('){
+                left++;
+//                cout << '(';
+            }
+            else{
+                if(left<=right) {
+                    left = -1;
+                    break;
+                }
+                right++;
+//                cout << ')';
+            }
+        }
+        if(left == right)
+            cout << "YES" <<'\n';
+        else
+            cout << "NO" <<'\n';
+        left =0;
+        right =0;
+
+    }
+
+
+}
