0df38d2fa9ad18e53ca49641f06be2e00be51c23
moon722 <miz722@khu.ac.kr>
2020-09-02 23:47:51 +0900

    9/2

--- baekjoon/graph/2331.cpp
+++ baekjoon/graph/2331.cpp
@@ -8,16 +8,40 @@
 
 /*
  * 반복수열
+ * RETRY
  */
 
 #include <iostream>
+#include <vector>
+#include <math.h>
 using namespace std;
-
+int P;
+int visit[1000001];
+vector<int> a;
+void dfs(int num){
+    visit[num]++;
+    a.push_back(num);
+    if(visit[num]==2)
+        return;
+    int next = 0;
+    for(auto elem : to_string(num))
+        next+=pow(elem-'0',P);
+    dfs(next);
+}
 int main(){
-    int A, P;
-    cin >> A >> P;
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    int A;
+    cin >> A >> P;ㅐ
+    dfs(A);
+    int result=0;
+    for(auto elem:a){
+        if(elem == a[a.size()-1])
+            break;
+        result++;
+    }
+    cout << result<<'\n';
 
 }
-
-
 
---
+++ baekjoon/graph/9466.cpp
@@ -0,0 +1,20 @@
+//
+// 9466.cpp
+// PS
+//
+// Created by mooninzoo on 9/2/20.
+// Copyright (c) 2020 mooninzoo. All rights reserved
+//
+
+/*
+ * 텀 프로젝트
+ * 한뱡향 원이거나 자기자신을 택할 때 제외
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+}
+
