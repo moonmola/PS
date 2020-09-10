709f1b8642af198cc6bf447130385986e29e4965
moon722 <miz722@khu.ac.kr>
2020-09-10 23:48:50 +0900

    9/10

--- baekjoon/DivideandConquer/11728.cpp
+++ baekjoon/DivideandConquer/11728.cpp
@@ -6,3 +6,37 @@
 // Copyright (c) 2020 mooninzoo. All rights reserved
 //
 
+/*
+ * 배열 합치기
+ * merge sort
+ */
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(n);
+    vector<int> b(m);
+    for(auto& elem:a)   cin >> elem;
+    for(auto& elem:b)   cin >> elem;
+    int ai=0,bi=0;
+    while(ai<n&& bi<m){
+        if(a[ai] < b[bi])
+            cout << a[ai++] <<' ';
+        else
+            cout << b[bi++] << ' ';
+    }
+    while(ai<n)
+        cout << a[ai++] << ' ';
+    while(bi<m)
+        cout << b[bi++] << ' ';
+
+}
+
+
---
+++ baekjoon/DivideandConquer/1780.cpp
@@ -0,0 +1,24 @@
+//
+// 1780.cpp
+// PS
+//
+// Created by mooninzoo on 9/10/20.
+// Copyright (c) 2020 mooninzoo. All rights reserved
+//
+
+/*
+ * 종이의 개수
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+
+
+}
