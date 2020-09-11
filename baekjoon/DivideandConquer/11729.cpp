159b9a939380d20589745ff439399fc5fd9074d5
moon722 <miz722@khu.ac.kr>
2020-09-11 23:39:11 +0900

    9/11

---
+++ baekjoon/DivideandConquer/11729.cpp
@@ -0,0 +1,13 @@
+//
+// 11729.cpp
+// PS
+//
+// Created by mooninzoo on 9/11/20.
+// Copyright (c) 2020 mooninzoo. All rights reserved
+//
+
+/*
+ * 하노이 탑 이동 순서
+ */
+
+#
--- baekjoon/DivideandConquer/1780.cpp
+++ baekjoon/DivideandConquer/1780.cpp
@@ -8,10 +8,35 @@
 
 /*
  * 종이의 개수
+ * RETRY
  */
 
 #include <iostream>
 using namespace std;
+int paper[2188][2188];
+int cnt[3];
+// cnt[0] : -1, cnt[1] : 0, cnt[2]:1로만 채워진 종이의 개수
+bool same(int x, int y, int n){
+    for(int i = x; i < x+n; i++){
+        for(int j = y; j <y+n; j++){
+            if(paper[x][y]!= paper[i][j])
+                return false;
+        }
+    }
+    return true;
+}
+void solve(int x, int y, int n){
+    if(same(x,y,n)){
+        cnt[paper[x][y]+1]++;
+        return;
+    }
+    int m = n/3;
+    for(int i = 0; i < 3; i ++){
+        for(int j = 0 ; j < 3; j++){
+            solve(x+i*m, y+j*m, m);
+        }
+    }
+}
 
 int main(){
     ios::sync_with_stdio(false);
@@ -19,6 +44,11 @@ int main(){
     cout.tie(nullptr);
     int n;
     cin >> n;
-
-
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n ; j++)
+            cin >> paper[i][j];
+    }
+    solve(0,0,n);
+    for(int i = 0; i < 3; i++) cout << cnt[i] << '\n';
+    return 0;
 }
